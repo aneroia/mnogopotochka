576660adc854ba2cb2d5b69c7c2722f381a6da6c
aneroia <95882423+aneroia@users.noreply.github.com>
2024-10-05 13:45:54 +0300

    Update pos.cpp

--- pos.cpp
+++ pos.cpp
@@ -1,11 +1,12 @@
-#include <pthread.h>
-#include <unistd.h>
 #include <atomic>
 #include <iostream>
 #include <memory>
 #include <mutex>
+#include <pthread.h>
 #include <queue>
 #include <sstream>
+#include <unistd.h>
+#include <vector>
 
 using namespace std;
 
@@ -13,18 +14,23 @@ queue<int> buffer;
 atomic<bool> data_ready(false);
 atomic<bool> finished(false);
 mutex mtx;
-pthread_mutex_t posix_mtx = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t tid_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int *thread_id = nullptr;
+vector<pthread_t> consumer_threads;
 
-int get_tid(int id) {
-  // 1 to 3+N thread ID
-  static thread_local shared_ptr<int> tid(new int);
-  if (id > 0) *tid = id;
-  return *tid;
+int get_tid() {
+  if (thread_id == nullptr) {
+    pthread_mutex_lock(&tid_mutex);
+    static int next_id = 1;
+    thread_id = new int(next_id++);
+    pthread_mutex_unlock(&tid_mutex);
+  }
+  return *thread_id;
 }
 
 void *producer_routine(void *arg) {
   (void)arg;
-  get_tid(2);
+  get_tid();
 
   string line;
   getline(cin, line);
@@ -44,35 +50,28 @@ void *producer_routine(void *arg) {
 }
 
 void *consumer_routine(void *arg) {
-  get_tid(3);
+  get_tid();
 
   int *sleep_limit = static_cast<int *>(arg);
   int sum = 0;
 
-  // Установка состояния отмены потока (разрешение отмены)
-  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
-  pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, nullptr);  // Отложенная отмена
-
   while (true) {
     int value;
 
-    {
-      lock_guard<mutex> lock(mtx);
-      if (!buffer.empty()) {
-        value = buffer.front();
-        buffer.pop();
-      } else {
-        if (finished) {
-          break;
-        }
-        continue;
-      }
+    lock_guard<mutex> lock(mtx);
+    if (buffer.empty() && finished) {
+      break;
+    }
+
+    if (!buffer.empty()) {
+      value = buffer.front();
+      buffer.pop();
+      data_ready = false;
+    } else {
+      continue;
     }
 
     sum += value;
-
-    // Проверяем возможность отмены потока (точка отмены)
-    pthread_testcancel();
 
     if (*sleep_limit > 0) {
       usleep((rand() % (*sleep_limit + 1)));
@@ -82,56 +81,51 @@ void *consumer_routine(void *arg) {
   return (void *)(size_t)sum;
 }
 
-
 void *consumer_interrupter_routine(void *arg) {
-  vector<pthread_t> *consumer_threads = static_cast<vector<pthread_t> *>(arg);
-  get_tid(4);  // Присваиваем идентификатор для потока interrupter
+  (void)arg;
+  get_tid();
 
   while (!finished) {
-    usleep(100000);  // Ожидание 100 мс
-
-    // Выбираем случайный поток потребителя для прерывания
-    int random_index = rand() % consumer_threads->size();
-
-    // Пробуем прервать выбранный поток потребителя
-    if (pthread_cancel((*consumer_threads)[random_index]) == 0) {
-      cout << "Interruptor cancelled consumer " << random_index + 1 << endl;
+    if (data_ready) {
+      int consumer_index = rand() % consumer_threads.size();
+      cout << "Interruptor is trying to interrupt consumer #" << consumer_index
+           << "..." << endl;
+      pthread_cancel(consumer_threads[consumer_index]);
     }
+
+    usleep(100000);
   }
 
   return nullptr;
 }
 
-
 void run_threads(int consumer_count, int sleep_limit) {
-  get_tid(1);
+  get_tid();
 
   pthread_t producer_thread;
-  vector<pthread_t> consumer_threads(consumer_count);
+  consumer_threads.resize(consumer_count);
   pthread_t interruptor_thread;
 
   pthread_create(&producer_thread, nullptr, producer_routine, nullptr);
 
   for (int i = 0; i < consumer_count; ++i) {
-    pthread_create(&consumer_threads[i], nullptr, consumer_routine, (void *)&sleep_limit);
+    pthread_create(&consumer_threads[i], nullptr, consumer_routine,
+                   (void *)&sleep_limit);
   }
 
-  pthread_create(&interruptor_thread, nullptr, consumer_interrupter_routine, (void *)&consumer_threads);
+  pthread_create(&interruptor_thread, nullptr, consumer_interrupter_routine,
+                 nullptr);
 
   int total_sum = 0;
   for (auto &consumer_thread : consumer_threads) {
     void *result;
     pthread_join(consumer_thread, &result);
-    if (result != PTHREAD_CANCELED) {
-      total_sum += (size_t)result;
-    } else {
-      cout << "Consumer thread was cancelled." << endl;
-    }
+    total_sum += (size_t)result;
   }
 
   pthread_join(producer_thread, nullptr);
+
   pthread_join(interruptor_thread, nullptr);
 
-  cout << "Total sum: " << total_sum << endl;
+  cout << total_sum << endl;
 }
-
